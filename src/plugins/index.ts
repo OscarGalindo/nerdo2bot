e22b684ebfe421dd894635cb09a60c97110f76d1
Foxandxss <foxandxss@gmail.com>
2016-04-21 13:21:31 +0200

    refactor reddit plugins into just one

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,16 @@
-import {TitsPlugin} from "./tits/TitsPlugin";
+import {RedditPlugin} from './reddit/RedditPlugin';
 import {GooglePlugin} from "./google/GooglePlugin";
-import {GatitosPlugin} from './gatitos/GatitosPlugin';
 
-export const PLUGINS = [new TitsPlugin(), new GooglePlugin(), new GatitosPlugin()];
+const tits = new RedditPlugin(
+  'http://www.reddit.com/r/legalteens+nipples+gonewild+nsfw+nsfw_gif+tits+realgirls/.json?',
+  'Tits Plugin',
+  'tits'
+);
+
+const gatitos = new RedditPlugin(
+  'http://www.reddit.com/r/catpictures+cats+CatGifs+StartledCats/.json?',
+  'Gatitos Plugin',
+  'gatitos'
+);
+
+export const PLUGINS = [tits, new GooglePlugin(), gatitos];
--- src/plugins/reddit/RedditPlugin.ts
+++ src/plugins/reddit/RedditPlugin.ts
@@ -1,14 +1,9 @@
 import {IPlugin} from "../../domain/plugin";
 import request = require("request-promise");
 
-export class TitsPlugin implements IPlugin {
-    private url = 'http://www.reddit.com/r/legalteens+nipples+gonewild+nsfw+nsfw_gif+tits+realgirls/.json?';
-
-    constructor(public name?:string, public command?:string) {
-        this.name = 'Tits Plugin';
-        this.command = 'tits';
-    }
-
+export class RedditPlugin implements IPlugin {
+    constructor(private url: string, public name: string, public command: string) { }
+    
     async exec(args:string[]) {
         return await request({uri: this.url, json: true})
             .then((data) => {
