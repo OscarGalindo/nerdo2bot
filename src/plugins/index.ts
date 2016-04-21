4c348104ec2365123804294084126c10fb605f26
Foxandxss <foxandxss@gmail.com>
2016-04-21 16:09:26 +0200

    now reddit caches 100 images

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,13 +3,13 @@ import {GooglePlugin} from "./google/GooglePlugin";
 import {MVLastPost} from "./mediavida/LastPost";
 
 const tits = new RedditPlugin(
-  'http://www.reddit.com/r/legalteens+nipples+gonewild+nsfw+nsfw_gif+tits+realgirls/.json?',
+  'http://www.reddit.com/r/legalteens+nipples+gonewild+nsfw+nsfw_gif+tits+realgirls/.json?limit=100',
   'Tits Plugin',
   'tits'
 );
 
 const gatitos = new RedditPlugin(
-  'http://www.reddit.com/r/catpictures+cats+CatGifs+StartledCats/.json?',
+  'http://www.reddit.com/r/catpictures+cats+CatGifs+StartledCats/.json?limit=100',
   'Gatitos Plugin',
   'gatitos'
 );
