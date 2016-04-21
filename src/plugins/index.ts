b5f867ca74cc48369f14332ebf0592f285733b04
Foxandxss <foxandxss@gmail.com>
2016-04-21 12:58:16 +0200

    add gatitos plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,4 +1,5 @@
 import {TitsPlugin} from "./tits/TitsPlugin";
 import {GooglePlugin} from "./google/GooglePlugin";
+import {GatitosPlugin} from './gatitos/GatitosPlugin';
 
-export const PLUGINS = [new TitsPlugin(), new GooglePlugin()];
+export const PLUGINS = [new TitsPlugin(), new GooglePlugin(), new GatitosPlugin()];
